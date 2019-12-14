ec97d53322fb6e26946253f7b29ed235c76623fb
magoyette <goyette.marcandre@gmail.com>
2019-12-14 14:54:10 -0500

    Allows to build multiple stories and switch between stories

--- README.md
+++ README.md
@@ -2,9 +2,39 @@
 
 A starter project for the [Chapbook](https://github.com/klembot/chapbook) Twine story format Chapbook. It uses the compiler [Tweego](https://www.motoslave.net/tweego/).
 
-The [Poof](https://github.com/ChapelR/poof) story format is used for proofing.
+**Features**
+
+- Build [Chapbook](https://github.com/klembot/chapbook) stories with the [Tweego](https://www.motoslave.net/tweego/) compiler
+- Proof stories with the [Poof](https://github.com/ChapelR/poof) story format
+- CSS files are linted by [Stylelint](https://stylelint.io/) and vendor prefixes are added by [Autoprefixer](https://github.com/postcss/autoprefixer)
+- [Sass](https://sass-lang.com/) can be used instead of CSS
+- JavaScript files are linted with [ESLint](https://eslint.org) and minimized with Babel
+- Live reload in the browser when files are changed
+- Build many stories with a single Node project
+
+**Warning**
 
 This project is a work in progress and has only been tested on Linux.
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+
+- [Installation](#installation)
+- [Build](#build)
+  - [Watching for changes with live reload](#watching-for-changes-with-live-reload)
+- [Proofing](#proofing)
+- [Working with multiple stories](#working-with-multiple-stories)
+  - [Switching between stories](#switching-between-stories)
+- [Tools](#tools)
+  - [Webpack](#webpack)
+  - [Babel](#babel)
+  - [ESLint](#eslint)
+  - [Node Sass](#node-sass)
+  - [Stylelint](#stylelint)
+  - [Autoprefixer](#autoprefixer)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
 
 ## Installation
 
@@ -73,6 +103,48 @@ npm run watch:poof
 # Starts a server with live reload on the Poof HTML file
 npm run start:poof
 ```
+
+## Working with multiple stories
+
+Multiple stories support allows to use Chapbook Tweego Starter to build multiple stories.
+
+That's useful if you want to store each story in a distinct Git repository, but keep using a single Node project to build them.
+
+Here's an example of a structure with many stories:
+
+- stories (folder ignored by the Git repository of chapbook-tweego-starter)
+  - story1 (the Git repository of a story)
+    - scripts
+    - styles
+    - twee
+  - story2 (the Git repository of another story)
+    - scripts
+    - styles
+    - twee
+- src (default story provided with chapbook-tweego-starter)
+
+The `stories` folder is configured to be ignored by the Git repository of chapbook-tweego-starter. The stories can be stored under `stories` each in their own repository.
+
+The `src` folder is used by default by Chapbook Tweego Starter. That example story is used for the purpose of testing Chapbook Tweego Starter.
+
+### Switching between stories
+
+The `switch` goal allows to configure which story should be built with chapbook-tweego-starter. Only one story can be built at a time.
+
+The argument passed to `npm run switch` is the path to the root folder of the story. That path must contains 3 subfolders: `scripts`, `styles` and `twee`.
+
+``` shell
+# Switch the current story to story1
+npm run switch stories/story1
+
+# Switch the current story to story2
+npm run switch stories/story2
+
+# Run goals like usual
+npm run build
+```
+
+That goal will set a NPM config variable named `chapbook-tweego-starter:storyPath` in the file `~/.npmrc`. That will override the variable `storyPath` in the `config` object of `package.json`. `storyPath` is set by default to `src`, which is the example story of Chapbook Tweego Starter.
 
 ## Tools
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,92 +2,97 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  // Two entries are necessary because the styles aren't linked to the JavaScript files
-  entry: ['./src/scripts/index.js', './src/styles/styles.scss'],
-  output: {
-    path: path.resolve(__dirname, 'dist/chapbook'),
-    filename: 'scripts.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        // pre indicates eslint will check files before the Babel transpilation
-        enforce: 'pre',
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        loader: 'eslint-loader',
-        options: {
-          // Set to true to prevent build from failing on errors
-          emitWarning: false
-        }
-      },
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        use: {
-          loader: 'babel-loader',
+module.exports = env => {
+  console.log('Story path: ' + env.storyPath);
+
+  return {
+    mode: 'production',
+    // Two entries are necessary because the styles aren't linked to the JavaScript files
+    entry: ['./' + env.storyPath + '/scripts/index.js',
+            './' + env.storyPath + '/styles/styles.scss'],
+    output: {
+      path: path.resolve(__dirname, 'dist/chapbook'),
+      filename: 'scripts.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          // pre indicates eslint will check files before the Babel transpilation
+          enforce: 'pre',
+          test: /\.js$/,
+          include: [
+            path.resolve(__dirname, env.storyPath)
+          ],
+          loader: 'eslint-loader',
           options: {
-            presets: [
-              ['@babel/preset-env', {
-                debug: false
-              }]
-            ]
+            // Set to true to prevent build from failing on errors
+            emitWarning: false
           }
-        }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
+        },
+        {
+          test: /\.js$/,
+          include: [
+            path.resolve(__dirname, env.storyPath)
+          ],
+          use: {
+            loader: 'babel-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
+              presets: [
+                ['@babel/preset-env', {
+                  debug: false
+                }]
+              ]
             }
           }
-        ]
-      }
-    ]
-  },
-  // Configure mini-css-extract-plugin to extract the CSS in a single file
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          include: [
+            path.resolve(__dirname, env.storyPath)
+          ],
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    // Configure mini-css-extract-plugin to extract the CSS in a single file
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true
+          }
         }
       }
-    }
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles.bundle.css'
-    }),
-    new StylelintPlugin({
-      context: 'src/styles',
-      // Set to true to prevent build from failing on errors
-      emitWarning: false
-    })
-  ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.bundle.css'
+      }),
+      new StylelintPlugin({
+        context: env.storyPath + '/styles',
+        // Set to true to prevent build from failing on errors
+        emitWarning: false
+      })
+    ]
+  };
 };
